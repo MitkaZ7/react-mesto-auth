0039c35494184ba347a548ee7016fff617b92628
MitkaS7 <mitka.dev@gmail.com>
2021-09-15 22:21:40 +0300

    clone project

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -11,7 +11,7 @@ class Api {
     }
   }
   getUserInfo() {
-    return fetch(`${this._url}/users/me`, {
+    return fetch(`${this._url}users/me`, {
       method: 'GET',
       headers: {
         authorization: this._token
@@ -20,7 +20,7 @@ class Api {
     .then(this._checkResponse)
   }
   getInitialCards() {
-    return fetch(`${this._url}/cards`, {
+    return fetch(`${this._url}cards`, {
       method: 'GET',
       headers: {
         authorization: this._token
@@ -29,7 +29,7 @@ class Api {
     .then(this._checkResponse)
   }
   editUserInfo(data) {
-    return fetch(`${this._url}/users/me`, {
+    return fetch(`${this._url}users/me`, {
       method:'PATCH',
       headers: {
         authorization: this._token,
@@ -43,7 +43,7 @@ class Api {
     .then(this._checkResponse)
   }
   editUserAvatar(data) {
-    return fetch(`${this._url}/users/me/avatar`, {
+    return fetch(`${this._url}users/me/avatar`, {
       method: 'PATCH',
       headers: {
         authorization: this._token,
@@ -56,7 +56,7 @@ class Api {
       .then(this._checkResponse)
   }
   addNewCard(data) {
-    return fetch(`${this._url}/cards`, {
+    return fetch(`${this._url}cards`, {
       method: 'POST',
       headers: {
         authorization: this._token,
@@ -70,7 +70,7 @@ class Api {
     .then(this._checkResponse)
   }
   likeCard(cardId, isLiked) {
-    return fetch(`${this._url}/cards/likes/${cardId}`, {
+    return fetch(`${this._url}cards/likes/${cardId}`, {
       method: `${!isLiked ? 'PUT' : 'DELETE'}`,
       headers: {
         authorization: this._token,
@@ -80,7 +80,7 @@ class Api {
     .then(this._checkResponse)
   }
   removeCard(cardId) {
-    return fetch(`${this._url}/cards/${cardId}`, {
+    return fetch(`${this._url}cards/${cardId}`, {
       method: 'DELETE',
       headers: {
         authorization: this._token,
@@ -90,5 +90,5 @@ class Api {
       .then(this._checkResponse)
   }
 }
-const api = new Api('https://nomoreparties.co/v1/cohort-26', '2ceacd5c-fca2-4318-a334-9732db7cb6fc');
+const api = new Api('https://nomoreparties.co/v1/cohort-26/', '2ceacd5c-fca2-4318-a334-9732db7cb6fc');
 export default api;
